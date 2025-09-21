9e5678b793a9306837c18ffacc12b129c9727406
The Gojo Find <cc25572@g.unicamp.br>
2025-09-21 20:28:10 -0300

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -7,79 +7,73 @@ const mssql = require('mssql');
 const cors = require('cors')
 
 app.use(cors({
-    origin:'*'
+    origin: '*'
 }))
 
 
 app.use(express.json())
 
-async function conectaBD(){
-    try{
+async function conectaBD() {
+    try {
         await mssql.connect(stringSQL);
     }
-    catch (error){
-        console.log('Algo deu errado na conexao do BD',error)
+    catch (error) {
+        console.log('Algo deu errado na conexao do BD', error)
     }
 }
 conectaBD();
 
+app.post('/Cadastro', async (req, res) => {
+    const Cadastro = await mssql.query(`insert into daroca.clientes (nome,email,celular,senha) VALUES ()`)
+})
+
+
+
+
 
 //http://localhost:8090/produtos
-app.get("/produtos" , async (req,res) => {
-    try{
-        const produtos = await mssql.query('SELECT * FROM daroca2.Produtos')
+app.get("/produtos", async (req, res) => {
+    try {
+        const produtos = await mssql.query('SELECT * FROM daroca.produtos')
         res.json(produtos.recordset)
-        console.log(produtos.recordset) 
-    }catch (error) {
+        console.log(produtos.recordset)
+    } catch (error) {
         console.error("Erro ao buscar produtos:", error);
         res.status(400).json({ erro: "Erro ao buscar produtos" });
     }
-    
+
 })
 
-app.get("/categorias" , async (req,res) => {
-    try{
-        const produtos = await mssql.query('SELECT * FROM daroca2.Categorias')
+app.get("/categorias", async (req, res) => {
+    try {
+        const produtos = await mssql.query('SELECT * FROM daroca.categorias')
         res.json(produtos.recordset)
-        console.log(produtos.recordset) 
-    }catch (error) {
+        console.log(produtos.recordset)
+    } catch (error) {
         console.error("Erro ao buscar produtos:", error);
         res.status(400).json({ erro: "Erro ao buscar produtos" });
     }
-    
+
 })
 
-app.get("/filtro/:value" , async (req,res) => {
+app.get("/filtrar/:value", async (req, res) => {
     const value = req.params.value;
-    
-    try{
-        const produtos = await mssql.query(`SELECT * FROM daroca2.Produtos WHERE categoria = ${value};`);
+    console.log(value)
+    try {
+        const produtos = await mssql.query(`SELECT * FROM daroca.produtos WHERE categoria = ${value}`);
         console.log(produtos.recordset)
         res.json(produtos.recordset)
     }
-    catch (error){
+    catch (error) {
         console.error("Erro ao buscar produtos:", erro);
         res.status(500).json({ erro: "Erro ao buscar produtos" });
     }
-    
+
 })
-
-
-app.get("/filtro_barra_de_pesquisa/:value", async (req,res) =>{
-    
-})
-
-
-
-
-
-
-
 
 
 // rota principal
-app.use('/', (req,res) => res.json( {mensagem: 'Servidor em execução'}) )
+app.use('/', (req, res) => res.json({ mensagem: 'Servidor em execução' }))
 
 // iniciar o servidor
 app.listen(porta, () => console.log("API funcionando!"))
-
