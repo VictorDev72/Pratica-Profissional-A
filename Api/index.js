5805e909af55ddf56047b7f3bc24b19fee57efd1
Rafael-Bcs <cc25346@g.unicamp.br>
2025-09-23 18:59:03 -0300

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,5 @@
 
+
 require("dotenv").config();
 const porta = process.env.PORTA;
 const stringSQL = process.env.CONNECTION_STRING;
@@ -34,7 +35,7 @@ app.post('/cadastro', async (req, res) => {
     try{
         let query = `insert into daroca.clientes (nome,email,celular,senha) VALUES ('${nome}','${email}','${celular}','${senha}')`
         await mssql.query(query)
-        res.status(201).json({'message':'Cadastro concluido'}) 
+        res.status(201).json({'message':'cadastro concluido'}) 
     }
     catch{
         res.status(500).json({'message':'Erro na inclusão dos dados'}) 
@@ -43,6 +44,9 @@ app.post('/cadastro', async (req, res) => {
 
 
 app.post('/login', async (req, res) => {
+    
+     
+    
     try {
         const nome = req.body.nome;
         const email = req.body.email;
@@ -51,10 +55,11 @@ app.post('/login', async (req, res) => {
         console.log(nome, email, celular, senha)
         const result = await mssql.query(`Select * FROM daroca.clientes where nome = '${nome}' AND senha = '${senha}'`)
         if(result.recordset.length > 0){
-            res.status(201).json({ "mensagem": "Login Valido" })
+            
+            res.status(200).json({ message: "Login Valido" })
         }
         else{
-            res.status(500).json({message : "Invalido"})
+            res.status(404).json({message : "Invalido"})
         }
     }
     catch (erro) {
@@ -66,12 +71,10 @@ app.post('/login', async (req, res) => {
 
 
 
-
-
 //http://localhost:8090/produtos
 app.get("/produtos", async (req, res) => {
     try {
-        const produtos = await mssql.query('SELECT * FROM daroca2.produtos')
+        const produtos = await mssql.query('SELECT * FROM daroca.produtos')
         res.json(produtos.recordset)
         console.log(produtos.recordset)
     } catch (error) {
@@ -83,7 +86,7 @@ app.get("/produtos", async (req, res) => {
 
 app.get("/categorias", async (req, res) => {
     try {
-        const categorias = await mssql.query('SELECT * FROM daroca2.categorias')
+        const categorias = await mssql.query('SELECT * FROM daroca.categorias')
         res.json(categorias.recordset)
         console.log(categorias.recordset)
     } catch (error) {
@@ -97,7 +100,7 @@ app.get("/filtrar/:value", async (req, res) => {
     const value = req.params.value;
     console.log(value)
     try {
-        const produtos = await mssql.query(`SELECT * FROM daroca2.produtos WHERE categoria = ${value}`);
+        const produtos = await mssql.query(`SELECT * FROM daroca.produtos WHERE categoria = ${value}`);
         console.log(produtos.recordset)
         res.json(produtos.recordset)
     }
