7162883ae33ef04b8279ff486d5e0ba175c91266
The Gojo Find <cc25572@g.unicamp.br>
2025-10-06 23:03:37 -0300

    Update routesClientes.js

--- Api_MVC/Routes/routesClientes.js
+++ Api_MVC/Routes/routesClientes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const controleClientes = require("Controller/controleClientes");
-const { addBD, login } = require("../Controller/controleClientes");
 
 
-router.post("/cadastro", addBD.inserir);
-router.post("/login", login.verificar);
+
+router.post("/cadastro", controleClientes.cadastro);
+router.post("/login", controleClientes.login);
 
 module.exports = router;
