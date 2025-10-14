905bdf2b206ef71e9047c2b62d91e6fc2e8634d3
The Gojo Find <cc25572@g.unicamp.br>
2025-10-14 16:44:18 -0300

    Update ModeloCliente.js

--- Api_MVC/Models/ModeloCliente.js
+++ Api_MVC/Models/ModeloCliente.js
@@ -1,15 +1,41 @@
 const bcrypt = require("bcrypt");
-const {mssql} = require("../Config/db.js")
+const {mssql} = require("../config/db.js");
+const { MAX } = require("mssql");
 
 
 
 async function cadastroCliente(cliente) {
-    const {prenome, sobrenome, email, celular ,nascimento, cpf, tipo, logradouro, numero, complemento, cep, senha} = cliente
-    console.log(cliente)
+    const {prenome, sobrenome, nascimento, email,celular, cpf, tipo, logradouro, numero, complemento, cep, senha} = cliente
+
     const senhaHash = await bcrypt.hash(senha, 10);
-    let query = `insert into daroca.cliente (prenome, sobrenome, email, celular ,nascimento, cpf, tipo, logradouro, numero, complemento, cep, senha)
-    VALUES ('${prenome}','${sobrenome}','${email}','${celular}','${nascimento}','${cpf}','${tipo}','${logradouro}','${numero}','${complemento}','${cep}','${senhaHash}')`
-    await mssql.query(query)
+
+    
+    try {
+        let query = `insert into daroca.cliente (prenome, sobrenome, nascimento, email, celular , cpf, tipo, logradouro, numero, complemento, cep, senha)
+        VALUES (@prenome, @sobrenome, @nascimento, @email, @celular, @cpf, @tipo, @logradouro, @numero, @complemento, @cep, @senhaHash)`
+
+        const request = new mssql.Request();
+
+        request.input('prenome', mssql.VarChar(20), prenome)
+        request.input('sobrenome', mssql.VarChar(30), sobrenome)
+        request.input('nascimento', mssql.Date, nascimento)
+        request.input('email', mssql.VarChar(50), email)
+        request.input('celular', mssql.VarChar(14), celular)
+        request.input('cpf', mssql.VarChar(15), cpf)
+        request.input('tipo', mssql.VarChar(10), tipo)
+        request.input('logradouro', mssql.VarChar(50), logradouro)
+        request.input('numero', mssql.VarChar(6), numero)
+        request.input('complemento', mssql.VarChar(20), complemento)
+        request.input('cep', mssql.VarChar(8), cep)
+        request.input('senha', mssql.VarChar(MAX), senha)
+
+        await request.query(query)
+
+    } catch (error) {
+        alert("Insira os dados de forma correta!!")
+    }
+    //console.log(cliente)
+    
 
     if (!senha) throw new Error("Senha não fornecida");
     return {mensagem: "Cadastro realizado com exito"}
