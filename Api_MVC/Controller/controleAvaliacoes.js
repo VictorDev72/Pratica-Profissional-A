f40d5ba2b08fe8720506f916df6ce274e2118b2a
Rafael-Bcs <cc25346@g.unicamp.br>
2025-11-17 19:07:08 -0300

    Add get50Aval function to controleAvaliacoes.js

--- Api_MVC/Controller/controleAvaliacoes.js
+++ Api_MVC/Controller/controleAvaliacoes.js
@@ -21,4 +21,13 @@ async function getAvalPorId(req,res) {
     
 }
 
-module.exports = {criarAval, getAvalPorId}
+async function get50Aval(req,res) {
+    try{
+        const result = await modAval.get50Aval()
+        res.status(200).json(result)
+    }catch(error){
+        res.status(400).json({error:"Erro ao buscar avaliações", error})
+    }
+}
+
+module.exports = {criarAval, getAvalPorId, get50Aval}
