3e1c3bb054529112e7b601f4a6f5ef4e4a3effe8
Rafael-Bcs <cc25346@g.unicamp.br>
2025-11-19 18:19:54 -0300

    Refactor ModeloAvaliacoes.js for improved database handling

--- Api_MVC/Models/ModeloAvaliacoes.js
+++ Api_MVC/Models/ModeloAvaliacoes.js
@@ -1,19 +1,20 @@
-const {mssql} = require("../config/db.js");
+const {mssql, stringSQL} = require("../config/db.js");
 const { MAX, query } = require("mssql");
 
 async function postAval(post) {
     const {idCliente, texto, nota} = post;
     try{
-        const query = "INSERT into daroca.avaliacao (idCliente, texto, nota) VALUES(@idCliente, @texto, @nota)"
-        const req = await mssql.Request();
+        const sql = "INSERT into daroca.avaliacao (idCliente, texto, nota) VALUES(@idCliente, @texto, @nota)"
+        await mssql.connect(stringSQL);
+        const req = new mssql.Request();
         req.input('idCliente', mssql.Int, idCliente);
         req.input('texto', mssql.VarChar(100), texto);
         req.input('nota', mssql.Int, nota);
 
-        await req.query(query)
-        return {mensagem: "Obrigado por avaliar"}
+        await req.query(sql)
+        return {mensagem: "Avaliação cadastrada com sucesso"}
     }catch(error){
-        console.error("Erro na inscerção dos dados: " + error.message);
+        console.log("Erro na inscerção dos dados: " + error.message);
         throw error;
     }
     
@@ -39,9 +40,15 @@ async function getAvalId(id) {
 }
 
 async function get50Aval() {
-    const query = " SELECT top 50 FROM daroca.Avaliacao join daroca.Cliente on avaliacao.idCliente = cliente.idCliente ORDER BY avaliacao.nota DESC"
-    result = await mssql.query(query);
-    return result;
+    try{
+        const query = "SELECT TOP(50) avaliacao.*, cliente.* FROM daroca.avaliacao JOIN daroca.Cliente ON avaliacao.idCliente = cliente.idCliente ORDER BY avaliacao.nota DESC"
+        result = await mssql.query(query);
+        return result;  
+    }catch(erro){
+        console.error("Erro na busca de dados: "+ erro.message);
+        throw erro;
+    }           
+    
 }
 
 module.exports = {postAval, getAvalId, get50Aval}
